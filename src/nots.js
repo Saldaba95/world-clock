c648ad818eac22673ced2867559903a2d450d5f8
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-17 20:06:41 -0600

    index/script/style/notes
    added some style new names for clock and replaced code and placed into notes section

---
+++ src/nots.js
@@ -0,0 +1,17 @@
+//setInterval(function () {
+//  let tokyoElement = document.querySelector("#tokyo");
+// let tokyoDateElement = tokyoElement.querySelector(".date");
+//  let tokyoTimeElement = tokyoElement.querySelector(".time");
+// let tokyoTime = moment().tz("Asia/Tokyo");
+
+// tokyoDateElement.innerHTM = tokyoTime.format("MMMM Do ,YYYY");
+//tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+//}, 1000);
+
+//let spainElement = document.querySelector("#spain");
+//let spainDateElement = spainElement.querySelector(".date");
+//let spainTimeElement = spainElement.querySelector(".time");
+//let spainTime = moment().tz("Europe/Paris");
+
+//spainDateElement.innerHTM = spainTime.format("MMMM Do ,YYYY");
+//spainTimeElement.innerHTML = spainTime.format("HH:MM:ss[<small>]A[</small>]");
--- src/script.js
+++ src/script.js
@@ -1,7 +1,20 @@
-let tokyoElement = document.querySelector("#tokyo");
-let tokyoDate = tokyoElement.querySelector(".date");
-let tokyoTime = tokyoElement.querySelector(".time");
-let currentTime = moment().tz("Asia/Tokyo");
-
-tokyoDate.innerHTM = currentTime.format("MMMM Do ,YYYY");
-tokyoTime.innerHTML = currentTime.format("HH:MM:SS [<small>]A[</small>]");
+function updateTime() {
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+  tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format(
+    "h:mm:ss[<small>] A [</small>]"
+  );
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = parisTime.format("dddd, Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
