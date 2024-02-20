815e6ab68b8df394c9b91f83fb5dade76032a86c
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-20 05:32:31 -0600

    Update script.js
    added a link

--- src/script.js
+++ src/script.js
@@ -39,7 +39,9 @@ function updateCity(event) {
             <div class="time">${cityTime.format(
               "h:mm:ss"
             )} <small>${cityTime.format("A")}</small></div>
-          </div>`;
+          </div>
+          <a href="index.html">Home</a>
+          `;
 }
 updateTime();
 setInterval(updateTime, 1000);
