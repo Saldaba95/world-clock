486c8ca88b16023dcc2284122ea8f8e7b08fa83f
Saldaba95 <aldaba.sarei1995@gmail.com>
2024-02-20 05:07:10 -0600

    index/script
    added ids and changed value
    changed the functionfor cities/names

--- src/script.js
+++ src/script.js
@@ -1,25 +1,29 @@
 function updateTime() {
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
-  tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format(
-    "h:mm:ss[<small>] A [</small>]"
-  );
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
+    tokyoDateElement.innerHTML = tokyoTime.format("dddd, Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss[<small>] A [</small>]"
+    );
+  }
   let parisElement = document.querySelector("#paris");
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  let parisTime = moment().tz("Europe/Paris");
-  parisDateElement.innerHTML = parisTime.format("dddd, Do YYYY");
-  parisTimeElement.innerHTML = parisTime.format(
-    "h:mm:ss [<small>] A [</small>]"
-  );
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+    parisDateElement.innerHTML = parisTime.format("dddd, Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>] A [</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  let cityName = cityTimeZone.replace("_ ", "").split("/")[1];
+  let cityName = cityTimeZone.replace("_ ", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
@@ -31,7 +35,7 @@ function updateCity(event) {
           <div>
             <div class="time">${cityTime.format(
               "h:mm:ss"
-            )}<small>${cityTime.format("A")}</small></div>
+            )} <small>${cityTime.format("A")}</small></div>
           </div>`;
 }
 updateTime();
